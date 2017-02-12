eae555b49c80727e0049e6158a76b3086686de55
David Brockman Smoliansky <david@springworks.se>
2017-02-12 22:19:29 +0100

    initial package

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # geodetic-haversine-distance
-Distance between two latitude/longitude coordinates taking latitude into account
+
+This function calculates the distance in meters between two latitude/longitude coordinates.
+Instead of using a mean radius of Earth, the radius is estimated using the `latitude` of the first position.
+
+
+## Install
+
+```
+$ npm install --save geodetic-haversine-distance
+```
+
+
+## Usage
+
+```js
+const haversineDistance = require('geodetic-haversine-distance');
+
+const a = { latitude: 59.338746, longitude: 18.060683 };
+const b = { latitude: 59.339042, longitude: 18.061779 };
+
+haversineDistance(a, b);
+//=> 70.37704048362993
+```
+
+
+## API
+
+### haversineDistance(start, end)
+
+Calculate the distance in meters between two positions.
+The two positions must have the properties `latitude` and `longitude`.
---
+++ __tests__/index.test.js
@@ -0,0 +1,29 @@
+const haversineDistance = require('..');
+
+describe('with two positions in Sweden', () => {
+
+  it('should give the distance in meters between two positions', () => {
+    const a = { latitude: 59.338746, longitude: 18.060683 };
+    const b = { latitude: 59.339042, longitude: 18.061779 };
+
+    const expected_distance = 70.377;
+
+    const distance = haversineDistance(a, b);
+    expect(distance).toBeCloseTo(expected_distance, 4);
+  });
+
+});
+
+describe('with two positions in Brazil', () => {
+
+  it('should give the distance in meters between two positions', () => {
+    const a = { latitude: 0.034199, longitude: -51.050784 };
+    const b = { latitude: 0.034055, longitude: -51.048738 };
+
+    const expected_distance = 228.3222;
+
+    const distance = haversineDistance(a, b);
+    expect(distance).toBeCloseTo(expected_distance, 4);
+  });
+
+});
